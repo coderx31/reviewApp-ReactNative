43437a5b3a54076df52e2547bf8272a3bffba433
coderx31 <shenal.fernando10@gmail.com>
2021-01-17 22:44:41 +0530

    create a global stylesheet

--- screens/home.js
+++ screens/home.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { StyleSheet, View, Text} from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default function Home(){
     return(
-        <View style={styles.container}>
-            <Text style={styles.text}>Home Screen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.text}>Home Screen</Text>
         </View>
-    )
+    );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-        
-    },
-    text:{
-        fontFamily: 'Poppins-regular',
-        fontSize: 24,
-    }
-})

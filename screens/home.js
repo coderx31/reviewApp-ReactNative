0b6f074eb0e5f44d4847c8ebbad3baaa98d4844f
coderx31 <shenal.fernando10@gmail.com>
2021-01-18 14:40:17 +0530

    done with navigation

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home(){
+export default function Home({ navigation }){
+
+    const pressHandler = () =>{
+        //navigation.navigate('ReviewDetail'); // first method
+        navigation.push('ReviewDetail'); // second method
+    }
+
     return(
         <View style={globalStyles.container}>
             <Text style={globalStyles.text}>Home Screen</Text>
+            <Button title='go to review dets ' onPress={pressHandler}/>
         </View>
     );
 }
--- screens/reviewDetail.js
+++ screens/reviewDetail.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetail(){
+export default function ReviewDetail({ navigation }){
+    const onPressHandler = () =>{
+        navigation.goBack();
+    }
     return(
         <View style={globalStyles.container}>
             <Text>ReviewDetail Screen</Text>
+            <Button title='back to home screen' onPress={onPressHandler}/>
         </View>
     )
 }

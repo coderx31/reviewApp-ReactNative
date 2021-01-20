c2ae1c31d2ae67dccf6338bf10922c91fc88792e
coderx31 <shenal.fernando10@gmail.com>
2021-01-20 17:40:25 +0530

    done with modal

--- screens/home.js
+++ screens/home.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Home({ navigation }){
+    const [modalOpen, setModalOpen] = useState(false);
     const [reviews, setReviews] = useState([
         {title: 'Zelda, breath of fresh air', rating: 5, body: 'lorem ipsum...', key: '1'},
         {title: 'Gotta catch them all (again)', rating: 4, body: 'lorem ipsum...', key: '2'},
@@ -14,11 +16,24 @@ export default function Home({ navigation }){
     return(
         <View style={globalStyles.container}>
 
-            <Modal visible={true}>
+            <Modal visible={modalOpen} animationType="slide">
                 <View style={styles.modalContent}>
-                    <Text>Hello From the modal</Text>
+                <MaterialIcons 
+                 name="close"
+                 size= {24}
+                 style={{...styles.modalToggle, ...styles.modalClose}}
+                 onPress = {() => setModalOpen(false)}
+                />
+                <Text>Hello From the modal</Text>
                 </View>
             </Modal>
+
+            <MaterialIcons 
+             name="add"
+             size= {24}
+             style={styles.modalToggle}
+             onPress = {() => setModalOpen(true)}
+            />
 
             <FlatList 
              data={reviews}
@@ -39,6 +54,19 @@ export default function Home({ navigation }){
 
 const styles = StyleSheet.create({
     modalContent:{
+        flex: 1,
         
+    },
+    modalToggle:{
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0,
     }
 });
